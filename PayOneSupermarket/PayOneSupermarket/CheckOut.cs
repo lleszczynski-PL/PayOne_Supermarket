c79e4bf5e74790e55baebf8a9a50dd8749a22bb9
Łukasz Leszczyński <lleszczynski@lbpro.pl>
2021-05-31 16:08:51 +0200

    public API for CheckOut from documentation

--- PayOneSupermarket/PayOneSupermarket/CheckOut.cs
+++ PayOneSupermarket/PayOneSupermarket/CheckOut.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PayOneSupermarket.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,33 @@ namespace PayOneSupermarket
 {
     public class CheckOut
     {
-        private CheckOut()
+        private readonly IEnumerable<PricingRule> pricingRules;
+
+        private int _total;
+
+        private CheckOut(IEnumerable<PricingRule> pricingRules)
+        {
+            this.pricingRules = pricingRules;
+        }
+
+        public static CheckOut NewCheckout(IEnumerable<PricingRule> pricingRules)
+        {
+            var checkout = new CheckOut(pricingRules);
+
+            return checkout;
+        }
+
+        public void Scan(Product product)
         {
 
+        }
+
+        public int Total
+        {
+            get
+            {
+                return _total;
+            }
         }
     }
 }
---
+++ PayOneSupermarket/PayOneSupermarket/DTO/PricingRule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayOneSupermarket.DTO
+{
+    public class PricingRule
+    {
+    }
+}
